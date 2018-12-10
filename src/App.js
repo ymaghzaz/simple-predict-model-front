c22fd5991ed85b342b11516a822d6be4fd742e7f
youssef maghzaz <youssefmaghzaz@gmail.com>
2018-12-10 12:13:00 +0100

    create hosting and upload file  ;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+
+const styles = {
+  App: {
+    width: "100%",
+    height: "400px",
+    backgroundColor: "green",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  Button: {
+    width: "200px",
+    height: "50px",
+    color: "red"
+  }
+};
 
 class App extends Component {
+  predict = () => {
+    axios
+      .get("https://9jacdgb3la.execute-api.eu-west-1.amazonaws.com/dev/hello")
+      .then(({ data }) => {
+        alert(JSON.stringify(data));
+      })
+      .catch(err => console.log(err));
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={styles.App}>
+        <button style={styles.Button} onClick={this.predict}>
+          Predict
+        </button>
       </div>
     );
   }
